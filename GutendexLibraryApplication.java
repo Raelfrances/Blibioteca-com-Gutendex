b59e87c7d3002b80a21d949afecfcb74da90ff19
Israel Alexandre Teófilo Santos <157400417+Raelfrances@users.noreply.github.com>
2025-08-17 20:03:52 -0300

    Create GutendexLibraryApplication.java

---
+++ GutendexLibraryApplication.java
@@ -0,0 +1,11 @@
+package com.example.gutendex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GutendexLibraryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GutendexLibraryApplication.class, args);
+    }
+}
